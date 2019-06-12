3c6056c0646da52d17665bb38b5cf1f581b2d572
Jake <jakecarating@gmail.com>
2019-06-12 17:12:30 -0400

    added test2

--- Lab1/test.sql
+++ Lab1/test.sql
@@ -1,1 +1,8 @@
-SELECT * FROM EMPLOYEES;
+
+select last_name, job_id 
+from employees
+where job_id  in (select job_id
+                from employees
+                where last_name = 'Davies')
+and last_name not like 'Davies';
+
